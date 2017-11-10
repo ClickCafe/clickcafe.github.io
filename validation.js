6b6fa44ca8cdf9acad9568bdf4644f602a6d6665
Scky1ar <christiankylesilvestre@outlook.com>
2017-11-10 14:48:49 +0800

    Update

--- validation.js
+++
@@ -1,8 +0,0 @@
-function validation(){
-    var name = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    
-    if( name !='' && password !=''){
-        alert("You logged in as: " + name);
-    }
-}
